6cc9d26bad4db096b1c8f2c97af88c020d305b0d
Crystal <cforourk@umich.edu>
2026-04-22 17:35:27 -0400

    added html form and linked in app. working and going to work on form submission

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-#get Flask libraries
-from flask import Flask
-#creating flask instance
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def home ():
-#test return browser text
-    return "Hello, Concert Program App"
-#is file being run and not imported
+def home():
+    return render_template('home.html')
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    # Get form data
+    data = request.form
+    print(data)  # For now, just print it
+    return "Form received!"
+
 if __name__ == '__main__':
     app.run(debug=True)
